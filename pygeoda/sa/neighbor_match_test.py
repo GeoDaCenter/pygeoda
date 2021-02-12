9130560d5a42647b889f2a1905d985c4f412046c
Xun Li <xunli@uchicago.edu>
2021-02-12 00:43:30 -0700

    clean up data

--- pygeoda/sa/neighbor_match_test.py
+++ pygeoda/sa/neighbor_match_test.py
@@ -3,6 +3,7 @@ __author__ = "Xun Li <lixun910@gmail.com>"
 from ..libgeoda import VecBool, VecVecBool
 from ..libgeoda import gda_neighbor_match_test
 from .lisa import lisa
+import math
 
 '''
 Changes:
@@ -25,7 +26,7 @@ def neighbor_match_test(geoda_obj, data, k, **kwargs):
         is_mile (bool, optional) TRUE (default) or FALSE, convert distance unit from mile to km.
 
     Returns:
-        dict: A dict with tuple values for keys "Cardinality" and "Probability".
+        dict: A dict with values for key "Cardinality" and "Probability".
     '''
     scale_method = 'standardize' if 'scale_method' not in kwargs else kwargs['scale_method']
     distance_method = 'euclidean' if 'distance_method' not in kwargs else kwargs['distance_method']
@@ -35,7 +36,13 @@ def neighbor_match_test(geoda_obj, data, k, **kwargs):
     is_mile =  False if 'is_mile' not in kwargs else kwargs['is_mile']
 
     result = gda_neighbor_match_test(geoda_obj.gda, k, power, is_inverse, is_arc, is_mile, data, scale_method, distance_method)
+    card = list(result[0])
+    prob = result[1]
+
+    # check p value
+    new_prob = [p if p >= 0 else math.nan for p in prob]
+
     return {
-        "Cardinality": result[0],
-        "Probability": result[1]
+        "Cardinality": card,
+        "Probability": new_prob
     }
