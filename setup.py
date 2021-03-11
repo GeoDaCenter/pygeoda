2ba625fb4d64924c2a7bb103fc8cfe11ab43a1af
Xun Li <lixun910@gmail.com>
2021-03-11 10:40:41 -0700

    bump to v0.0.7.3

--- setup.py
+++ setup.py
@@ -206,7 +206,7 @@ extensions = [Extension('pygeoda._libgeoda',
                         extra_objects=EXTRA_OBJECTS),]
 
 setup (name = 'pygeoda',
-       version = '0.0.7.1',
+       version = '0.0.7.3',
        author = "Xun Li",
        author_email = "lixun910@gmail.com",
        url = "https://github.com/geodacenter/pygeoda",
