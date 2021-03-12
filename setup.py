0e9e044c6a4da853259cc784b1ddace9729858bb
Xun Li <xunli@uchicago.edu>
2021-03-11 20:33:37 -0700

    bump to v0.0.7.4

--- setup.py
+++ setup.py
@@ -202,7 +202,7 @@ extensions = [Extension('pygeoda._libgeoda',
                         extra_objects=EXTRA_OBJECTS),]
 
 setup (name = 'pygeoda',
-       version = '0.0.7.3',
+       version = '0.0.7.4',
        author = "Xun Li",
        author_email = "lixun910@gmail.com",
        url = "https://github.com/geodacenter/pygeoda",
