1f5bdec0b5b0d4dd3aa2b9ea8aa3b656817cf3e8
Xun Li <xunli@uchicago.edu>
2021-08-05 16:25:48 -0700

    add read spatial weights functions; update libgeoda

--- pygeoda/weights/__init__.py
+++ pygeoda/weights/__init__.py
@@ -5,4 +5,4 @@ from .queen import queen_weights
 from .rook import rook_weights
 from .distance import distance_weights, min_distthreshold, knn_weights
 from .kernel import kernel_weights, kernel_knn_weights
-from .weight import Weight
+from .weight import Weight, read_gal, read_gwt, read_swm
